bda00b83e8c9451186ab684c6b36d27fddcbcc83
Abe <ab.b2k3@gmail.com>
2021-08-25 11:31:14 +0200

    commit 39:13

--- week1/hello.c
+++ week1/hello.c
@@ -1,9 +1,9 @@
-/*in progress: https://youtu.be/zYierUhIFNQ?t=1805*/
+/*in progress: help50 - https://youtu.be/zYierUhIFNQ?t=2353*/
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     string answer = get_string("What's you name? ");
-    printf("Hello, %s", answer);
+    printf("Hello, %s\n", answer);
 }

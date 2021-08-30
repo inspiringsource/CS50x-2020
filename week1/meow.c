c36710e8d63a4dec8b0d96fe7bd9adc5133cd314
Abe <ab.b2k3@gmail.com>
2021-08-30 21:12:18 +0200

    complete Lecture 1

---
+++ week1/meow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//prototype
+void meow(int n);
+
+
+int main(void)
+{
+    meow(5);
+}
+
+void meow(int n){
+    for(int i=0; i<n; i++){
+    printf("meow\n");
+    }
+}
---
+++ week1/positive.c
@@ -0,0 +1,25 @@
+// Abstraction and scope
+ 
+#include <cs50.h>
+#include <stdio.h>
+ 
+ //prototype
+int get_positive_int(string prompt);
+ 
+int main(void)
+{
+    int i = get_positive_int("Positive integer: ");
+    printf("%i\n", i);
+}
+ 
+// Prompt user for positive integer
+int get_positive_int(string prompt)
+{
+    int n;
+    do
+    {
+        n = get_int("%s", prompt);
+    }
+    while (n < 1);
+    return n;
+}

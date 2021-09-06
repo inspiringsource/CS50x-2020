a310fda28ef2c1d1854ddb66089e4cde3a7ff729
Abe <ab.b2k3@gmail.com>
2021-09-06 18:00:52 +0200

    valgrind

---
+++ week4/copy2.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    char *t = malloc(strlen(s) + 1);
+    if(t==NULL){
+        return 1; // Quit, let's get out of the program
+    }
+
+    strcpy(t, s);
+
+    if(strlen(t)>0){
+    *t = toupper(*t); //the same t[0] = toupper(t[0]);
+    }
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+    
+    free(t); //free memory
+}

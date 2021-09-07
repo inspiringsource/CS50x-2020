b4b33aeeb022d4f8c82a51a11b13738ef8e662bc
Abe <ab.b2k3@gmail.com>
2021-09-07 19:43:55 +0200

    complete Lecture 4

--- week4/scanf2.c
+++ week4/scanf2.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    char s[6]; //CS50 library automatically allocates memory to scanf
-    printf("s: ");
-    scanf("%s", s);
+    char s[6]; //Here (without CS50.h library) we have to declare an array size [6]
+    printf("s: "); //with the CS50 library allocation of memory is automatic
+    scanf("%s", s); 
     printf("s: %s\n", s);
 }

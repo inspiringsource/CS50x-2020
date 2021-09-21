83495e27246c60fdafd740b26cee2d61fd3ecda7
Abe <ab.b2k3@gmail.com>
2021-09-21 13:48:09 +0200

    update probelm4

--- Projecteuler/python/problem4.py
+++ Projecteuler/python/problem4.py
@@ -9,7 +9,7 @@ print("Answer: ", end='')
 #for i in range(10, 100):
 #for i in range(100, 10, -1):
 #for i in range(1000, 100, -1):
-for i in range(100, 1000):
+for i in range(100, 1001):
     if target % i == 0:
         print(i, end='; ')
         answers.append(i)
